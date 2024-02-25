5e4b620bba0c3b4bdd90fbdca945a0e633928a91
Camille <83260908+Camille846@users.noreply.github.com>
2024-02-24 22:05:07 -0300

    Feat: conditional logic to dynamic questions

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,12 +1,66 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import questions from '../../../assets/data/questions.json';
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
+    title: string = '';
+    questions: any;
+    questionSelected: any;
+    answers: string[] = [];
+    answerSelected: string = '';
+    answerDescription: string = '';
+    questionIndex: number = 0;
+    questionMaxIndex: number = 0;
+    finished: boolean = false;
 
+    constructor () {}
+
+    ngOnInit() {
+       if (questions) {
+          this.finished = false;
+          this.title = questions.title;
+          this.questions = questions.questions;
+          this.questionSelected = this.questions[this.questionIndex];
+          this.questionIndex = 0;
+          this.questionMaxIndex = this.questions.length - 1;
+       }
+
+      }
+      
+      selectOption(value: string) {
+         this.answers.push(value);
+         this.nextQuestion();
+      }
+
+      async nextQuestion() {
+        if (this.questionIndex < this.questionMaxIndex) {
+          this.questionIndex++;
+          this.questionSelected = this.questions[this.questionIndex];
+        } else {
+          const finalAnswer:string = await this.checkAnswer(this.answers);
+          this.finished = true;
+          this.answerSelected = questions.results[finalAnswer as keyof typeof questions.results].title;
+        }
+      }
+
+      async checkAnswer(answers: string[]) {
+         const result = answers.reduce((prev, curr, i, arr) => { 
+            if (arr.filter(item => item === curr).length > 
+            arr.filter(item => item == curr).length) {
+                return prev;
+            } else {
+                return curr;
+            }
+         })
+
+         return result;
+      }
+
+      
 }
