a508dab07c1eeba66065ff9719406763d57aef9e
Camille <83260908+Camille846@users.noreply.github.com>
2024-02-25 21:36:47 -0300

    Fix: remove unnecessary page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { QuizComponent } from '../../components/quiz/quiz.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [QuizComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
